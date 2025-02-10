16b292e6007494f09aa1d20259b5de33436dce50
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-02-10 18:24:11 +0000

    update

--- philo/src/time_management.c
+++ philo/src/time_management.c
@@ -25,20 +25,12 @@ unsigned long	get_time_ms(t_sim *sim)
 void	usleep_limit(unsigned long time_ms, t_sim *sim)
 {
 	unsigned long	act_start;
-	unsigned long	elapsed;
-	unsigned long	remaining;
 
 	act_start = get_time_ms(sim);
 	while ((get_time_ms(sim) - act_start) < time_ms)
 	{
 		if (!sim_active(sim))
 			break ;
-		elapsed = get_time_ms(sim) - act_start;
-		remaining = time_ms - elapsed;
-		if (remaining > 1)
-			usleep((remaining * 1000) / 2);
-		else
-			while ((get_time_ms(sim) - act_start) < time_ms)
-				;
+		usleep(500);
 	}
 }
